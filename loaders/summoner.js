01df013d481ae4a7549a495ca9c89f8a02426b86
Andrew Hyndman <ajhyndman@hotmail.com>
2016-12-18 00:13:17 +1100

    Refactor: Improve efficiency of SummonerLoader

--- loaders/summoner.js
+++ loaders/summoner.js
@@ -4,11 +4,11 @@ import fetch from 'node-fetch';
 import key from './key';
 
 
-const getSummonerByName = (region) => (name) =>
-  fetch(`https://${region}.api.pvp.net/api/lol/${region}/v1.4/summoner/by-name/${name}?${key}`)
+const getSummonersByName = (region) => (names) =>
+  fetch(`https://${region}.api.pvp.net/api/lol/${region}/v1.4/summoner/by-name/${names.join(',')}?${key}`)
     .then(response => response.json())
-    .then(json => json[name.toLowerCase()]);
+    .then(json => Object.values(json));
 
 export default (region) => new DataLoader(
-  names => Promise.all(names.map(getSummonerByName(region)))
+  getSummonersByName(region)
 );
