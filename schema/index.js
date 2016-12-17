66e773716e6eafd72f268f81f1ffe787a856f176
Andrew Hyndman <ajhyndman@hotmail.com>
2016-12-17 20:08:51 +1100

    Add Query support for champions

--- schema/index.js
+++ schema/index.js
@@ -1,13 +1,15 @@
 import {
+  GraphQLEnumType,
+  GraphQLList,
   GraphQLNonNull,
   GraphQLObjectType,
   GraphQLSchema,
   GraphQLString,
-  GraphQLEnumType,
   graphql,
 } from 'graphql';
 import fetch from 'node-fetch';
 
+import ChampionType from './types/champion';
 import RegionType from './types/region';
 import SummonerType from './types/summoner';
 import { RIOT_API_KEY } from '../secret';
@@ -30,12 +32,24 @@ const QueryType = new GraphQLObjectType({
         },
         region: {
           type: new GraphQLNonNull(RegionType),
-        }
+        },
       },
       resolve: (root, { name, region }) =>
         fetch(`https://${region}.api.pvp.net/api/lol/${region}/v1.4/summoner/by-name/${name}?${KEY_PARAM}`)
           .then(response => response.json())
           .then(json => json[name.toLowerCase()]),
+    },
+    champions: {
+      type: new GraphQLList(ChampionType),
+      args: {
+        region: {
+          type: new GraphQLNonNull(RegionType),
+        },
+      },
+      resolve: (root, { region }) =>
+        fetch(`https://${region}.api.pvp.net/api/lol/static-data/${region}/v1.2/champion?champData=all&${KEY_PARAM}`)
+          .then(response => response.json())
+          .then(json => Object.values(json.data)),
     },
   }),
 });
