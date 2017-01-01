e52b976cf9290b71b4f4e9fdcc6bd118c467bf09
Matthew Hyndman <myndman@gmail.com>
2017-01-01 23:48:32 +1100

    remove some log statements and make implementation functional, though not all fields are returned for each queue

--- src/loaders/queueStats.js
+++ src/loaders/queueStats.js
@@ -4,26 +4,20 @@ import { find, propEq } from 'ramda';
 import fetch from '../fetch';
 import key from './key';
 
+
+// todo: have an int or enum variable for season id
 const season = 'SEASON2017';
 
 const getQueueStats = (region) => (id) =>
     fetch(`https://${region}.api.pvp.net/api/lol/${region}/v1.3/stats/by-summoner/${id}/summary?${key}&season=${season}`)
       .then(response => response.json())
-      .then(json => (console.log("almost: gqs reponse: ", json.playerStatSummaries), json.playerStatSummaries))
-      .catch(err => { throw err; });
-d
-//console.log("almost:",getQueueStats("na")(21913759);
+      .then(json => (json.playerStatSummaries))
+     .catch(err => { throw err; });
 
 // find+propEq finds the first queueStats for the given mode or undefined
 export default (region) => (mode) => {
   const qsf = getQueueStats(region);
-  const qsf_and_find = (id) => find(propEq('playerStatSummaryType', mode))(qsf(id));
-  //console.log("almost: queue stats:", qs);
-  const f = (ids) => (console.log("almost: ids are: ", ids), console.log("almost: qsf ids are:", ids.map(qsf_and_find)), Promise.all(ids.map(qsf_and_find))); // function from list ids to list of promises
+  const qsf_and_find = (id) => qsf(id).then(qstats => find(propEq('playerStatSummaryType', mode))(qstats));
+  const f = (ids) => (Promise.all(ids.map(qsf_and_find))); // function from list ids to list of promises
   return new DataLoader(f);
 };
-
-
-//new DataLoader(
-//  ids => Promise.all(ids.map(find(propEq('playerStatSummaryType', mode))(getQueueStats(region))))
-//);
