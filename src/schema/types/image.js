05c4d1a377bbc4dd539c861193a14059c729ae5d
Andrew Hyndman <ajhyndman@hotmail.com>
2017-01-16 20:54:18 +1100

    Generate useful URLs in ImageData

--- src/schema/types/image.js
+++ src/schema/types/image.js
@@ -5,14 +5,25 @@ import {
 } from 'graphql';
 
 
+// TODO: This should probably not be hardcoded to a specific version.
+const HOST = 'http://ddragon.leagueoflegends.com/cdn/7.1.1/img/';
+
 export default new GraphQLObjectType({
   name: 'Image',
   description: 'This object contains image data',
   fields: () => ({
-    full: { type: GraphQLString },
+    full: {
+      type: GraphQLString,
+      resolve: (imageData) =>
+        `${HOST}champion/${imageData.full}`
+    },
     group: { type: GraphQLString },
     h: { type: GraphQLInt },
-    sprite: { type: GraphQLString },
+    sprite: {
+      type: GraphQLString,
+      resolve: (imageData) =>
+        `${HOST}sprite/${imageData.sprite}`,
+    },
     w: { type: GraphQLInt },
     x: { type: GraphQLInt },
     y: { type: GraphQLInt },
